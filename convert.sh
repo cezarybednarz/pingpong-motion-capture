521cfcd2a5b7e7ea2c91e6184f6f59f20a1a2ffc
Cezary Bednarz <cezary.bednarz@gmail.com>
2019-12-13 11:32:54 +0100

    comment in convert.sh

--- convert.sh
+++ convert.sh
@@ -13,5 +13,5 @@ do
     echo $i
     ./filter "frames/frame$i.bmp"
 done
-
+# maybe -crf 60, or change -r
 ffmpeg -r 60 -f image2 -s 1920x1080 -i frames/frame%04d.bmp -vcodec libx264 -crf 25  -pix_fmt yuv420p test_wynik.mp4
