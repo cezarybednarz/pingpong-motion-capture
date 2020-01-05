dbbdb6a87b8e61a5588f46c6ecff40ef46a1b060
cezary <cezary.bednarz@students.mimuw.edu.pl>
2020-01-05 01:02:26 +0100

    merge xd

--- convert.sh
+++ convert.sh
@@ -7,15 +7,15 @@ rm frames/frame*.bmp
 
 make
 
-ffmpeg -ss 00:00:04 -t 00:00:10 -i test_film.mp4 -r 30.0 frames/frame%4d.bmp
+ffmpeg -ss 00:00:00 -t 00:00:04 -i film.mp4 -r 30.0 frames/frame%4d.bmp
 
-for i in $(seq -f "%04g" 1 180)
+for i in $(seq -f "%04g" 1 120)
 do
     echo $i
     ./filter "frames/frame$i.bmp"
 done
-rm test_wynik.mp4
+rm wynik.mp4
 # maybe -crf 60, or change -r
-ffmpeg -r 30 -f image2 -s 1920x1080 -i frames/frame%04d_converted.bmp -vcodec libx264 -crf 25  -pix_fmt yuv420p test_wynik.mp4
+ffmpeg -r 30 -f image2 -s 1920x1080 -i frames/frame%04d_converted.bmp -vcodec libx264 -crf 25  -pix_fmt yuv420p wynik.mp4
 
 echo essa
